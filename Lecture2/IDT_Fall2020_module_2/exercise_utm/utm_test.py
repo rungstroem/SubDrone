341fe6fbaac7928d03e896e9e5351a642ca6ad99
Kenneth <rungstroem@hotmail.com>
2020-10-19 17:08:24 +0200

    t

--- Lecture2/IDT_Fall2020_module_2/exercise_utm/utm_test.py
+++ Lecture2/IDT_Fall2020_module_2/exercise_utm/utm_test.py
@@ -1,43 +1,4 @@
 #!/usr/bin/env python3
-#*****************************************************************************
-# UTM projection conversion test
-# Copyright (c) 2013-2020, Kjeld Jensen <kjeld@frobomind.org>
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#    * Redistributions of source code must retain the above copyright
-#      notice, this list of conditions and the following disclaimer.
-#    * Redistributions in binary form must reproduce the above copyright
-#      notice, this list of conditions and the following disclaimer in the
-#      documentation and/or other materials provided with the distribution.
-#    * Neither the name of the copyright holder nor the names of its
-#      contributors may be used to endorse or promote products derived from
-#      this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
-# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#*****************************************************************************
-"""
-This file contains a simple Python script to test the UTM conversion class.
-
-Revision
-2013-04-05 KJ First version
-2015-03-09 KJ Minor update of the license text.
-2016-01-16 KJ Corrected a minor problem with the library location reference.
-2020-02-03 KJ Python 3 compatible, changed functionality: it now moves a
-              geodetic position 100 meter East
-2020-09-17 KJ Changed first line to python3
-"""
-
 # import utmconv class
 from utm import utmconv
 from math import pi, cos, acos, sin, atan2, sqrt
